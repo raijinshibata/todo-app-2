d0d2a58fcec8b5634839f891166ce60149d24280
raijin.shibata <raijin.shibatar@MacBook-Pro-4.local>
2018-08-07 19:42:08 +0900

    フォームで書き直した

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 //app.js
 var express = require('express');
-var ejs = require('ejs');
-
+var ejs = require("ejs");
+  
 var app = express();
-app.engine('ejs', ejs.renderFile);
+app.engine('ejs',ejs.renderFile);
+app.use(express.static('public'));
+
+var bodyParser = require('body-parser'); //★追加
+app.use(bodyParser.urlencoded({extended: false})); //★追加
 
 
 var PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.render('index.ejs',
-  	{title: 'Index',
-  		content: 'This is Express-app Top page!'});
+// ※トップページ
+app.get("/",(req, res) => {
+    var msg = 'This is Index Page!<br>' 
+        + 'Write a message.';
+    var url = '/other?name=taro&pass=yamada';
+    res.render('index.ejs', 
+        {
+            title: 'Index', 
+            content: msg, 
+            //link:{href:url, text:'※別のページに移動'}
+        });
 });
+
+app.post('/',(req, res) => {
+    var msg = 'This is Posted Page!<br>' +
+        'あなたは「<b>' + req.body.message + 
+        '</b>」と送信しました。';
+    res.render('index.ejs', 
+        {
+            title: 'Posted', 
+            content: msg, 
+        });
+});
+
 
 var sarver = app.listen(PORT, () =>{
 	console.log('Server is runnning!');
