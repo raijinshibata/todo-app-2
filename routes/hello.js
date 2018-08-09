bc7463f3d66bc9de592fbdd662f1f8f442711e70
raijin.shibata <raijin.shibatar@MacBook-Pro-4.local>
2018-08-09 16:06:32 +0900

    dbのURLをheroku用に設定

--- routes/hello.js
+++ routes/hello.js
@@ -5,8 +5,10 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const assert = require('assert');
 
-const db_url = 'mongodb://127.0.0.1:27017/todoDB';
-
+//ローカル用DB URL
+//const db_url = 'mongodb://127.0.0.1:27017/todoDB';
+// heroku用DB URL
+const db_url =  ' mongodb://heroku_3ds27242:gi4da16afn0r2a5p3n87k8p8h1@ds129811.mlab.com:29811/heroku_3ds27242'
 
 
 var data_list = [];
