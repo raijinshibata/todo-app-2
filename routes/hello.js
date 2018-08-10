768b6d2aa20897dba99005f9cf8c246195549653
raijin.shibata <raijin.shibatar@MacBook-Pro-4.local>
2018-08-10 13:01:15 +0900

    初期完成

--- routes/hello.js
+++ routes/hello.js
@@ -11,17 +11,39 @@ const db_url = 'mongodb://127.0.0.1:27017/toDB';
 //const db_url =  'mongodb://heroku_3ds27242:gi4da16afn0r2a5p3n87k8p8h1@ds129811.mlab.com:29811/heroku_3ds27242'
 
 //ローカル用DB名
-const db_name = 'test_db';
+const db_name = 'todoDB';
 //heroku用DB名
 //const db_name = 'heroku_3ds27242';
 
 
 router.get('/',(req, res, next) => {
-    var data = {
-        title: 'Hello!',
-        content: 'やる事を追加する場合は入力してください'
-    };
-    res.render('hello', data);
+
+        MongoClient.connect(db_url, (err, client) => {
+        assert.equal(null, err)
+        const db = client.db(db_name);
+        //console.log("Connected successfully to server")
+        db.collection('todo_list', (err, collection) =>{
+
+
+
+            //データベースからデータを取ってくる
+            collection.find().toArray((err, items) =>{
+                var db_data = items;
+                console.log(db_data);
+                var data = {
+                    title: 'todo-list',
+                    content: db_data
+                };
+                res.render('hello', data);
+            });
+
+        });
+        // db.collection("user").deleteMany({'name' : 'yamada'}, function(err, result) {
+        //     if (err) throw err;
+        //     console.log("delete");
+        // });
+
+    });
 });
 
 router.post('/post',(req, res, next) => {
@@ -33,43 +55,76 @@ router.post('/post',(req, res, next) => {
     MongoClient.connect(db_url, (err, client) => {
         assert.equal(null, err)
         const db = client.db(db_name);
-        console.log("Connected successfully to server")
+        //console.log("Connected successfully to server")
         db.collection('todo_list', (err, collection) =>{
 
-        //入力された値をデータベースに格納
-        var new_data = req.body['new_data'];
-        var del_list = req.body.item;
-        console.log(del_list);
-        try{
-            //console.log(Number(req.body.item[0]));
-            //１個しか消さないとき配列にする
-            if(Array.isArray(del_list)==false){
-                del_list = [del_list];
-            }
-            for(var i in del_list){
-                collection.deleteMany({'todo' : del_list[i]}, function(err, result) {
-                    console.log(del_list[i] + " is deleted.");
-                });
-            }
-        } catch(err){
+            //入力された値をデータベースに格納
+            var new_data = req.body.new_data;
+            var del_list = req.body.item;
+            console.log(new_data);
+            console.log(del_list);
 
-        }
-        if(new_data ==null || new_data==""){
-            //入力事項が空なら何も送信しない
-        }else{
-            collection.insert({todo: new_data}, (err,result) =>{
 
-            });
-        }
             //データベースからデータを取ってくる
             collection.find().toArray((err, items) =>{
                 var db_data = items;
-                //console.log(db_data);
-                var data = {
-                    title: 'todo-list',
-                    content: db_data
-                };
-                res.render('hello_post', data);
+                console.log(db_data);
+
+                if (typeof new_data === 'undefined'){
+                //チェック更新
+                    try{
+                        //console.log(Number(req.body.item[0]));
+                        //１個しか消さないとき配列にする
+                        if(Array.isArray(del_list)==false){
+                            del_list = [del_list];
+                        }
+                        for(var i in del_list){
+                            //チェックつけたものは完了とする
+                            collection.update({'todo' : del_list[i]}, {$set:{'completed' : true}});
+                            //collection.deleteMany({'todo': del_list[i]});
+                        }
+                        collection.find().toArray((err, items) =>{
+                            var db_data = items;
+                            var data = {
+                                title: 'todo-list',
+                                content: db_data
+                            };
+                            res.render('hello_post', data);
+
+                            client.close();      //db.close()じゃないよ
+                        });
+                    } catch(err){
+                    }
+                } else{
+                //リストを更新する処理
+                    if(new_data ==null || new_data==""){
+                        //入力事項が空なら何も送信しない
+                        var data = {
+                            title: 'todo-list',
+                            content: db_data
+                        };
+                        res.render('hello_post', data);
+
+                        client.close();
+
+                    }else{
+                        collection.insert({todo: new_data, completed: false}, (err,result) =>{
+                            //データベースからデータを取ってくる
+                            collection.find().toArray((err, items) =>{
+                                var db_data = items;
+                                var data = {
+                                    title: 'todo-list',
+                                    content: db_data
+                                };
+                                res.render('hello_post', data);
+
+                                client.close();      //db.close()じゃないよ
+                            });
+                        });
+                    }
+                }
+
+
             });
 
         });
@@ -78,7 +133,6 @@ router.post('/post',(req, res, next) => {
         //     console.log("delete");
         // });
 
-        client.close();      //db.close()じゃないよ
     });
 });
 
